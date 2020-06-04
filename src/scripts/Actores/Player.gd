b02262f9a7fac79163fbc6618e8b1d8b12e38ee8
Caresle <carabesle@gmail.com>
2020-06-03 20:45:45 -0600

    Trabajando en un nuevo movimiento
    El movimiento ahora toma encuenta aceleracion y friccion para mover al
    personaje.
    
    Se empieza con el nuevo movimiento del salto.

--- src/scripts/Actores/Player.gd
+++ src/scripts/Actores/Player.gd
@@ -5,29 +5,42 @@ const SUELO := Vector2.UP
 
 export var velocidad : int = 0
 export var aceleracion : float = 0
+export var friccion : float = 0
 export var gravedad := 98
+export var salto := 100
 
 var movimiento := Vector2.ZERO
 var distancia : float = 0
 var direccion := 0
+var puedo_saltar : bool = false
 
 func _ready() -> void:
 	$".".modulate = Colores.COLOR_JUGADOR
 
 
-func _physics_process(delta: float) -> void:
-	mover(delta)
+func _physics_process(_delta: float) -> void:
+	mover()
 
 
-func mover(delta) -> void:
-	
+func mover() -> void:
 	direccion = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	
-	distancia = velocidad * delta
-	movimiento.x = (distancia * direccion) / delta
-	movimiento.y += gravedad * delta
+	if direccion > 0:
+		movimiento.x = min(movimiento.x + aceleracion, velocidad)
+	if direccion < 0:
+		movimiento.x = max(movimiento.x - aceleracion, -velocidad)
+	else:
+		movimiento.x = lerp(movimiento.x, 0, friccion)
+	mover_y()
+	
+	movimiento = move_and_slide(movimiento, SUELO)  
+
+
+func mover_y() -> void:
+	movimiento.y += gravedad * get_physics_process_delta_time()
 	
 	if is_on_floor():
 		movimiento.y = 0
-	
-	movimiento = move_and_slide(movimiento, SUELO)
+		puedo_saltar = true
+	if Input.is_action_just_pressed("ui_salto"):
+		movimiento.y = -salto
