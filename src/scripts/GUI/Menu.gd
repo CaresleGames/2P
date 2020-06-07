51ca77cb28f189be5912f03bd11ece9aa814f62f
Caresle <carabesle@gmail.com>
2020-06-07 14:54:25 -0600

    problemas de autoload resuelto

--- src/scripts/GUI/Menu.gd
+++ src/scripts/GUI/Menu.gd
@@ -5,6 +5,7 @@ export var siguiente_escena : PackedScene
 
 func _on_Iniciar_pressed() -> void:
 	get_tree().change_scene_to(siguiente_escena)
+	ControlJuego.inicio_juego = true
 
 
 func _on_Salir_pressed() -> void:
--- src/scripts/autoload/ControlJuego.gd
+++ src/scripts/autoload/ControlJuego.gd
@@ -3,23 +3,31 @@ extends Node
 signal reaparecer_inicio
 signal reinicio
 
-onready var jugador = get_tree().get_nodes_in_group("player")[0]
-onready var enemigo = get_tree().get_nodes_in_group("enemigo")[0]
-onready var gui_gameover = get_tree().get_nodes_in_group("game_over")[0]
+var jugador
+var enemigo 
+var gui_gameover 
+var inicio_juego := false
+
 
 func _ready() -> void:
 	connect("reaparecer_inicio", self, "_reaparecer_inicio")
 	connect("reinicio", self, "_reinicio")
 
 func _process(_delta: float) -> void:
-	
+	if inicio_juego:
+		jugador = get_tree().get_nodes_in_group("player")[0]
+		if has_node("Enemigo"):
+			enemigo = get_tree().get_nodes_in_group("enemigo")[0]
+		if has_node("GameOver"):
+			gui_gameover = get_tree().get_nodes_in_group("game_over")[0]
+		if jugador.vidas <= 0:
+			gui_gameover.show()
+			jugador.hide()
+			enemigo.hide()
 	if Input.is_action_just_pressed("ui_accept"):
 		var value = get_tree().reload_current_scene()
 		print(value)
-	if jugador.vidas <= 0:
-		gui_gameover.show()
-		jugador.hide()
-		enemigo.hide()
+	
 
 
 func _reaparecer_inicio() -> void:
