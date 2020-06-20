05a6ecf96ec997c850842ef319d722ef0b63edb8
Caresle <carabesle@gmail.com>
2020-06-20 11:27:42 -0600

    vidas en pantalla

--- src/scripts/NivelEtiqueta.gd
+++ src/scripts/NivelEtiqueta.gd
@@ -1,6 +1,16 @@
 extends Label
 
+var vidas : int 
 
 func _ready() -> void:
 	var texto := get_parent().name
 	$".".text = texto
+
+
+func _process(_delta: float) -> void:
+	if ControlJuego.jugador.vidas == null:
+		print("null")
+	else:
+		vidas = ControlJuego.jugador.vidas
+	$Vidas.text = str(vidas)
+	print("vidas: " + str(vidas))
