5e794408d845246865069cd3044e60574a5c032d
Caresle <carabesle@gmail.com>
2020-06-25 19:21:10 -0600

    Intro

--- src/escenas/Intro.gd
+++ src/escenas/Intro.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
+export var menu : PackedScene
+
+
 func _ready() -> void:
 	yield(get_tree().create_timer(.5), "timeout")
 	$Anim.play("Intro")
+
+
+func _process(_delta: float) -> void:
+	
+	if Input.is_action_just_pressed("ui_salto"):
+		$Anim.play("Salida")
+
+
+func _on_Anim_animation_finished(anim_name: String) -> void:
+	if anim_name == "Salida":
+		print("Salida")
+		get_tree().change_scene_to(menu)
