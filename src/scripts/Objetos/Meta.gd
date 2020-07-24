2f7aa055b03263dd8c66ca4bed1fc16a21f63eae
Caresle <carabesle@gmail.com>
2020-07-23 19:08:21 -0600

    Contador de vida

--- src/scripts/Objetos/Meta.gd
+++ src/scripts/Objetos/Meta.gd
@@ -4,9 +4,17 @@ export var siguiente_nivel : PackedScene
 
 func _on_Meta_body_entered(_body: KinematicBody2D) -> void:
 	var anim : AnimationPlayer
+	var barra_vidas : Node2D
+	var barra_vidas_anim : AnimationPlayer
+	if get_tree().get_nodes_in_group("BarraVidas").size() > 0:
+		barra_vidas = get_tree().get_nodes_in_group("BarraVidas")[0]
+		barra_vidas_anim = barra_vidas.get_node("Anim")
 	if get_tree().get_nodes_in_group("trans").size() > 0:
 		var trans = get_tree().get_nodes_in_group("trans")[0]
 		anim = trans.get_child(1)
+	if not barra_vidas_anim == null:
+		barra_vidas_anim.play("Salida")
+		yield(barra_vidas_anim, "animation_finished")
 	if not anim == null:
 		anim.play("Entrada")
 		yield(anim, "animation_finished")
